07cb5b930d66caadd7bf0dc47981eaf85147d52f
Heedong Lee <arkinylee@gmail.com>
2014-12-05 00:22:12 +0900

    120514 Adds some comments

--- 120414 ConnectedComponents/CCProblem/cParser.cpp
+++ 120414 ConnectedComponents/CCProblem/cParser.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <queue>
 
 #include "cParser.h"
 
@@ -9,6 +10,7 @@ using namespace std;
 
 cParser::cParser()
 {
+	// init line database
 	m_vLines = new vector < char* > ;
 	m_nLineNums = 0;
 }
@@ -16,6 +18,7 @@ cParser::cParser()
 
 cParser::~cParser()
 {
+	// dest all
 	delete[] visited;
 	char *pt;
 	while (!m_vLines->empty()){
@@ -27,26 +30,38 @@ cParser::~cParser()
 
 
 void cParser::loadFromFile(const char* fileName){
+	// 최초 설계
 	// 해당 숫자만큼의 bool 배열을 만든다.
 	// 1을 읽고 bool 배열을 true로 만든다.
 	// known person의 주소가 true인지 확인한다.
 	// true가 아니면 
 	// known person을 큐에 넣는다.
-	// 디큐한다.
-	// known person의
+	// 디큐한다. 
 	
+	// read file to buffer
 	fstream read(fileName);
 	//int lines;
+	// read data size from buffer
 	read >> m_nLineNums;
+	// init bool array for visit information
 	visited = new bool[m_nLineNums];
 
+	// make buffer chunk, but first line will not use.
 	char* in = new char[10000];
 	read.getline(in, 10000);
+	delete in;
+	
+	// loop 'til data numbers
 	for (int i = 0; i < m_nLineNums; i++){
+		//make buffer chunk
 		char* in = new char[10000];
 		read.getline(in, 10000);
+		// pushback into vector
 		m_vLines->push_back(in);
+		// recheck, visited will be allocated in zero based.
 		visited[i] = false;
+
+
 		//int id;
 		//read >> id;
 		//int numofknown;
@@ -61,21 +76,29 @@ void cParser::loadFromFile(const char* fileName){
 		//}
 		//m_vStoredVector->push_back(input);
 	}
-
+	
+	// for check human number
 	int nhuman = 0;
+	// get first dataline from the vector.
+	// condition : first line of data should be always true.
+	// DataBase Format (p2-1.txt, p2-2.txt)
+	// ID numKnown ID1 ID2 ....
 	string dataline(m_vLines->at(0));
 	stringstream stream(dataline);
 	
-
 	int id;
 	int numKnown;
 
+	// queue for not checked list.
+	// this queue will store unvisited people who are human.
 	queue<int> workinglist;
 	stream >> id;
+	// recheck, visiting informations are allocated ID-1 index.
 	visited[id-1] = true;
+	// first human added
 	nhuman++;
 	stream >> numKnown;
-
+	// enqueue the first human list which the first human knows.
 	for (int i = 0; i < numKnown; i++){
 		int input;
 		stream >> input;
@@ -87,19 +110,26 @@ void cParser::loadFromFile(const char* fileName){
 		}
 	}
 
+	// while queue is not empty
 	while (workinglist.size() != 0){
+		// Get a front person ID and get his human list.
 		int currentpeople = workinglist.front();
 		workinglist.pop();
 
+		// If he has not yet be visited,
 		if (visited[currentpeople - 1] != true){
 			stringstream stream(m_vLines->at(currentpeople - 1));
 			stream >> id;
+			// change the visiting information that this person is visited.
 			visited[id - 1] = true;
 			stream >> numKnown;
+			// human size up!
 			nhuman++;
+			// Add unvisited humen which is from ID's list to working queue 
 			for (int i = 0; i < numKnown; i++){
 				int input;
 				stream >> input;
+				// if the ID hasn't yet visited,
 				if (visited[input - 1] != true){
 					workinglist.push(input);
 				}
@@ -110,6 +140,9 @@ void cParser::loadFromFile(const char* fileName){
 		}
 	}
 
+	// after the workdone,
+	// check visited people who are humen from the visiting information
+	// for double checking
 	int doublecheck = 0;
 	for (int i = 0; i < m_nLineNums; i++){
 		if (visited[i] == true) {
@@ -148,13 +181,14 @@ void cParser::print(){
 }
 
 void cParser::printHuman(){
-	int doublecheck = 0;
+	//int doublecheck = 0;
 	for (int i = 0; i < m_nLineNums; i++){
 		if (visited[i] == true) {
+			// print list of the Human
 			cout << i + 1 << " ";
-			doublecheck++;
+			//doublecheck++;
 		}
 	}
 	cout << endl;
-	cout << "human num double check : " << doublecheck << endl;
+	//cout << "human num double check : " << doublecheck << endl;
 }
--- 120414 ConnectedComponents/CCProblem/cParser.h
+++ 120414 ConnectedComponents/CCProblem/cParser.h
@@ -1,17 +1,18 @@
 #pragma once
 #include <vector>
-#include <queue>
-#include <map>
+//#include <queue>
+//#include <map>
 
-struct Person{
-	int nID;
-	std::vector<int> vKnownPerson;
-	bool visited = false;
-};
+//struct Person{
+//	int nID;
+//	std::vector<int> vKnownPerson;
+//	bool visited = false;
+//};
 
 class cParser
 {
 public:
+	// init parser
 	cParser();
 	~cParser();
 
@@ -20,8 +21,11 @@ public:
 	void printHuman();
 
 private:
+	// Size of Data
 	int m_nLineNums;
+	// Store Raw Data per Line from File
 	std::vector<char*>* m_vLines;
+	// Visited Informations
 	bool *visited;
 };
 
